35969988759168cc3f687b5bd1de8f3358c46c47
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2025-04-04 16:30:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn=document.querySelector("input");
+btn.addEventListener(("click"),()=>{
+let sel=document.getElementById("colorSelect");
+let selectedIndex=sel.selectedIndex;
+if(selectedIndex!=-1)
+{
+sel.remove(selectedIndex);
+}
+});
